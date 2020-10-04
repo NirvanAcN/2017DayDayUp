b94a95224266f18ccc974c70b95c1a533a1714d6
马浩萌 <1652370555@qq.com>
2020-10-04 23:00:34 +0800

    restart my 2020 and my life

--- Day2-ResponderChain/Day2-ResponderChain/UIResponderResponderChainExtension.swift
+++ Day2-ResponderChain/Day2-ResponderChain/UIResponderResponderChainExtension.swift
@@ -28,7 +28,7 @@ extension UIResponder {
     ///   - identifier: 事件的id（根据id判断当前UIResponder是否处理此事件）
     ///   - sender: 事件所传递的数据
     /// - Returns: 若不继续向下传递return true事件被拦截，响应链中断；继续传递return false，直到responderRouterActionChain返回true或者传递到UIApplication仍未被响应
-    func responderRouterActionChain(by identifier: String, sender: Any!) -> Bool {
+    @objc dynamic func responderRouterActionChain(by identifier: String, sender: Any!) -> Bool {
         return false
     }
 }
